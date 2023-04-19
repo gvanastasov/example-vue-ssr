867bb2294bc2b265f0c333b38bf25bbad891b439
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-04-19 12:44:15 +0200

    add ejs for main template reponse

--- src/server/server.js
+++ src/server/server.js
@@ -11,29 +11,13 @@ const __dirname = path.dirname(__filename);
 const server = express();
 const port = process.env.PORT || 8080;
 
-server.get('/', (req, res) => {
+server.set('view engine', 'ejs');
+
+server.get('/', (_req, res) => {
     const app = createApp();
   
     renderToString(app).then((html) => {
-      res.send(`
-      <!DOCTYPE html>
-      <html>
-        <head>
-          <title>Vue SSR Example</title>
-          <script type="importmap">
-            {
-              "imports": {
-                "vue": "https://unpkg.com/vue@3/dist/vue.esm-browser.js"
-              }
-            }
-          </script>
-          <script type="module" src="/client.js"></script>
-        </head>
-        <body>
-          <div id="app">${html}</div>
-        </body>
-      </html>
-      `);
+      res.render(path.join(__dirname, './index.ejs'), { html })
     });
   });
 

49230426b2c3b5e57fa2d8df482e1a77f403632c
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-04-20 09:58:33 +0200

    improve ui

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,5 +1,13 @@
 <template>
-    <div @click="count++">{{ count }}</div>
+    <div style="display: flex; align-items: center; justify-content: center; height: 100vh;">
+        <div style="text-align: center;">
+            <h1>Vue SSR Application</h1>
+            <p>a dummy application that renders on the server and rehydrates on the client.</p>
+            <p>Counter: {{ count }}</p>
+            <button @click="count++">Increase</button>
+            <button @click="count--">Decrease</button>
+        </div>
+    </div>
 </template>
 
 <script>

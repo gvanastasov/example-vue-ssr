26772335b6442a2d842d1195c62ec6a96d954d14
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-04-20 15:38:33 +0200

    adjust app to show router exp

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,11 +1,24 @@
 <template>
     <div style="display: flex; align-items: center; justify-content: center; height: 100vh;">
-        <div style="text-align: center;">
+        <div style="text-align: center; max-width: 400px;">
             <h1>Vue SSR Application</h1>
-            <p>a dummy application that renders on the server and rehydrates on the client.</p>
-            <p>Counter: {{ count }}</p>
-            <button @click="count++">Increase</button>
-            <button @click="count--">Decrease</button>
+            <p>a dummy application that renders on the server and rehydrates on the client. Try the reactivity with the buttons bellow, to increase and decrease the counter value. You can also inspect the source of the page, where you should see the SSR result. Further down the page, you can also try out routing, which is handled both on the server but as well on the client.</p>
+            
+            <section>
+                <h2>Reactivity</h2>
+                <p>Counter: {{ count }}</p>
+                <button @click="count++">Increase</button>
+                <button @click="count--">Decrease</button>
+            </section>
+
+            <section>
+                <h2>Routing</h2>
+                <div style="margin-bottom: 10px;">
+                    <router-link :to="{ name: 'home' }" style="margin-right:10px">Home</router-link>
+                    <router-link :to="{ name: 'about' }">About</router-link>
+                </div>
+                <router-view></router-view>
+            </section>
         </div>
     </div>
 </template>

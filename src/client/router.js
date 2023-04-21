bfbd3311e073bbd49a1ed91140e06fe5fcd97423
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-04-21 10:07:34 +0200

    add runtime server routing table map

--- src/client/router.js
+++ src/client/router.js
@@ -3,24 +3,28 @@ import { createRouter, createMemoryHistory, createWebHistory } from 'vue-router'
 import Home from './pages/Home.vue';
 import About from './pages/About.vue';
 
-export const createSSRRouter = function({ isServer }) {
+const routes = [
+    {
+        name: 'home',
+        path: '/',
+        component: Home
+    },
+    {
+        name: 'about',
+        path: '/about',
+        component: About
+    }
+];
+
+const createSSRRouter = function({ isServer }) {
     let history = isServer ? createMemoryHistory() : createWebHistory()
     
     const router = createRouter({
         history,
-        routes: [
-            {
-                name: 'home',
-                path: '/',
-                component: Home
-            },
-            {
-                name: 'about',
-                path: '/about',
-                component: About
-            }
-        ]
+        routes
     })
 
     return router;
-}
+};
+
+export { routes, createSSRRouter };
--- src/server/server.js
+++ src/server/server.js
@@ -4,14 +4,49 @@ import { fileURLToPath } from 'url';
 
 import { renderToString } from 'vue/server-renderer';
 import { createApp } from '../client/app.js'
+import { routes } from '../client/router.js'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const server = express();
 const port = process.env.PORT || 8080;
+const publicLocation = express.static(path.join(__dirname,'../../dist/public'));
 
-const routeHandler = function (context) {
+/**
+ * Generates server routes out of client side routing engine.
+ * @param {*} route 
+ * @param {*} parent 
+ */
+const generateServerRoutes = function(route, parent) {
+  let routePath = `${route.path}`;
+  if (parent) {
+    routePath = path.join(parent.path, routePath)
+  }
+
+  generateServerRoute(routePath);
+
+  if (route.children && route.children.length) {
+    route.children.forEach(x => generateServerRoutes(x, route))
+  }
+}
+
+/**
+ * Registers route in the server routing engine.
+ * @param {*} routePath 
+ */
+const generateServerRoute = function(routePath) {
+  console.log(`Generating runtime location at: ${routePath}`)
+  server.get(routePath, handleServerRouting);
+}
+
+/**
+ * Sets the application's state based on current request url, by matching it
+ * a route configured inside the client router. 
+ * @param {*} context 
+ * @returns 
+ */
+const routeApplication = function (context) {
   return new Promise((resolve, reject) => {
     const { app, router } = createApp({ isServer: true })
 
@@ -31,29 +66,27 @@ const routeHandler = function (context) {
   })
 }
 
+/**
+ * Common handling function that server side renders a vue routed application,
+ * and injects that into a common template ejs. 
+ * @param {*} req 
+ * @param {*} res 
+ */
+function handleServerRouting(req, res) {
+  const context = { url: req.url };
+
+  // todo: error handling missing
+  routeApplication(context).then(app => {
+    renderToString(app).then((html) => {
+      res.render(path.join(__dirname, './index.ejs'), { html });
+    });
+  });
+}
+
+routes.forEach(x => generateServerRoutes(x, null));
+
 server.set('view engine', 'ejs');
-
-// todo: we should probably use router obj to determine routes, and not
-// use a catch all...
-server.get('/', (req, res) => {
-  const context = { url: req.url }
-  routeHandler(context).then(app => {
-    renderToString(app).then((html) => {
-      res.render(path.join(__dirname, './index.ejs'), { html })
-    });
-  })
-});
-
-server.get('/about', (req, res) => {
-  const context = { url: req.url }
-  routeHandler(context).then(app => {
-    renderToString(app).then((html) => {
-      res.render(path.join(__dirname, './index.ejs'), { html })
-    });
-  })
-})
-
-server.use(express.static(path.join(__dirname,'../../dist/public')));
+server.use(publicLocation);
 
 server.listen(port, () => {
   console.log('Server started at http://localhost:' + port);
